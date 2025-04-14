87660e2a81c6eff8336abb028b15c6f0d6b7754d
Barry-owino <barryowino5@gmail.com>
2025-04-14 23:22:57 +0300

    added the coming soon banner for testing purposes

--- portfolio/main_app/views.py
+++ portfolio/main_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class ComingSoonView(TemplateView):
+    template_name = 'main_app/coming_soon.html'
