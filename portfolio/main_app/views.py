04523b27454a3a650c93aa02b206e02ca7481000
Barry-owino <barryowino5@gmail.com>
2025-04-15 00:19:40 +0300

    created views to hundle projects,user and contactmessage

--- portfolio/main_app/views.py
+++ portfolio/main_app/views.py
@@ -1,6 +1,27 @@
 #from django.shortcuts import render
 from django.views.generic import TemplateView
+from rest_framework import generics
+from .models import UserProfile, Project, ContactMessage
+from .serializers import UserProfileSerializer, ProjectSerializer, ContactMessageSerializer
 
 # Create your views here.
 class ComingSoonView(TemplateView):
     template_name = 'main_app/coming_soon.html'
+
+
+class UserProfileView(generics.ListAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+class ProfileListCreateView(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class ContactMessageCreateView(generics.CreateAPIView):
+    queryset = ContactMessage.objects.all()
+    serializer_class = ContactMessageSerializer
+
